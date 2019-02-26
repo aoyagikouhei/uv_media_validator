3571b9552b500d327a9f7781cc77a3f576f2b310
aoyagikouhei <aoyagi.kouhei@gmail.com>
2019-02-26 09:47:10 +0900

    modify fb image 10MB

--- lib/uv_media_validator/fb_image.rb
+++ lib/uv_media_validator/fb_image.rb
@@ -3,6 +3,9 @@ require 'image_size'
 module UvMediaValidator
   # https://developers.facebook.com/docs/graph-api/photo-uploads/
   class FbImage
+    include UvMediaValidator::Validator::FileSize
+
+    MAX_SIZE = 10_000_000
     FORMAT_ARRAY = %i(jpeg png gif bmp tiff)
 
     def initialize(path, info: nil)
@@ -14,12 +17,16 @@ module UvMediaValidator
       @image_size ||= ImageSize.path(@path)
     end
 
+    def file_size
+      @file_size ||= FileTest.size?(@path)
+    end
+
     def format?
       FORMAT_ARRAY.include?(image_size.format)
     end
 
     def all?
-      format?
+      file_size? && format?
     end
   end
 end
